7eacdf781841b381b98d1b78edfad2332253887c
918 <lnxbyu@outlook.com>
2024-07-09 19:39:59 +0800

    feat: esc退出编辑模式

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,21 +1,37 @@
 'use client';
 
-import React from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
 import styles from './index.module.css';
 import { IDBData } from '@/services/home';
 import CardList from '@/components/CardList';
 import { getSelectedKey } from '@/common';
 import Head from '@/components/Head';
+import { PageContext } from '@/context/page.context';
 
 export interface IProps {
   dbData: IDBData;
 }
 
 const Main = (props: IProps) => {
+  const { setEditCardMode } = useContext(PageContext);
   const configKey = getSelectedKey();
   const selectedConfig = props.dbData?.[configKey];
   const { layout, dataSource = [] } = selectedConfig || {};
   const { cardListStyle, head } = layout || {};
+
+  const onKeydown = useCallback((e: any) => {
+    if (e && e.keyCode === 27) {
+      // Esc button
+      setEditCardMode?.(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', onKeydown);
+    return () => {
+      document.body.removeEventListener('keydown', onKeydown);
+    };
+  }, [onKeydown]);
 
   return (
     <main
@@ -35,3 +51,6 @@ const Main = (props: IProps) => {
 };
 
 export default Main;
+function setEditCardMode(arg0: boolean) {
+  throw new Error('Function not implemented.');
+}
